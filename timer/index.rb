2b7a9a0179f09e0ee51e06b0cc9d1385006d43fd
luk3skyw4lker <lucashenriqueblemos@gmail.com>
2022-01-22 18:14:07 -0300

    [feat]: Build a timer

---
+++ timer/index.rb
@@ -0,0 +1,14 @@
+def timer (start)
+	puts "Minutes: " + start.to_s
+	start_time = Time.now
+
+	puts start_time.strftime("Start to_time: %I:%M:%S %p")
+
+	start.downto(1) { |i| sleep 60 }
+
+	end_time = Time.now
+
+	puts end_time.strftime("Elapsed time: %I:%M:%S %p")
+end
+
+timer(10)
